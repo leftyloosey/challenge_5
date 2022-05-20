8fa047930d260f676b897a6a3dcfee3ad971650e
davidhardin <thisgoestodavid@gmail.com>
2022-05-19 18:50:41 -0600

    huge progress on the script, and some minor tweaking to css. index just to reflect those changes. and got rid of the assignment instructions because didn't need anymore.

--- script.js
+++ script.js
@@ -1,58 +1,67 @@
 var now = moment()
-// var hour = now.format("h")
-//var hour = 3
-$("#currentDay").text(now.format("MMM Do, YYYY"));
+var hour = now.format("H")
 
-$("#currentHour").text(now.format("h:mm:ss a"));
-$("#currentHour").text(now.format("h:mm:ss a"));
-$("#currentH").text(hour);
+var input9 = []
+var input10 = []
+var input11 = []
+var input12 = []
+var input1 = []
+var input2 = []
+var input3 = []
+var input4 = []
+var input5 = []
 
 
-console.log("hello")
-if ($("#row2").attr("data-hour") === hour) {
-    console.log("yes, it equals hour")
-} else console.log("nope")
-
+$("#currentDay").text(now.format("MMM Do, YYYY"))
+$("#currentHour").text(now.format("h:mm a"))
 
 
 const hourRows = {
-    row9: $("#row9").attr("data-hour"),
-    row10: $("#row10").attr("data-hour"),
-    row11: $("#row11").attr("data-hour"),
-    row12: $("#row12").attr("data-hour"),
-    row1: $("#row1").attr("data-hour"),
-    row2: $("#row2").attr("data-hour"),
-    row3: $("#row3").attr("data-hour"),
+    row9: $("#row9"),
+    row10: $("#row10"),
+    row11: $("#row11"),
+    row12: $("#row12"),
+    row1: $("#row1"),
+    row2: $("#row2"),
+    row3: $("#row3"),
     row4: $("#row4"),
-    row5: $("#row5").attr("data-hour"),
+    row5: $("#row5"),
+}
+console.log(hour)
+function nowColor() {Object.keys(hourRows).forEach(key => {
+    if ($(hourRows[key]).attr("data-hour") == hour) {
+        ($(hourRows[key]).attr("class", "table-secondary"))
+    }
+    else if ($(hourRows[key]).attr("data-hour") < hour) {
+        ($(hourRows[key]).attr("class", "table-warning"))
+    }
+    else if ($(hourRows[key]).attr("data-hour") > hour) {
+        ($(hourRows[key]).attr("class", "table-success"))
+        }
+})}
+
+$("#btn9").click(function(){
+    clickFunction($('#event9'), $('#input9'), input9)
+})
+
+function clickFunction(a,b,c) {
+    c.push($(b).val())
+    console.log(c, "in function")
+    $(a).html(c.join("<br>"))
+    $(b).val('')
+    $(a).append(b)
+    // localStorage.setItem('stored9', JSON.stringify(c))
+    // var storage = localStorage.getItem(JSON.stringify('stored9'))
+    // console.log(storage)
+    localStorage.setItem('stored9', c)
+    var storage = localStorage.getItem('stored9')
+    console.log(storage)
+
 }
 
+function init() {
+    $('#event9').text(localStorage.getItem('stored9'))
+}
 
-
-// Object.values(hourRows).forEach(val => {
-//     console.log(val)
-//     if (val == hour ) {
-//         console.log(val, " is the current hour")
-
-//     }
-// });
-
-// Object.keys(hourRows).forEach(key => {
-//     if (hourRows[key] == hour) {
-//         console.log("blah blah ", key)
-//         hourRows[key] 
-//     }
-
-// })
-
-//$("#row9").attr("class", "table-success")
-
-var hour = 4
-
-$.each(hourRows, function(key, value) {
-    if (value == hour) {
-        console.log("ok", value, key)
-    $(key).attr("class", "table-success")
-    }
-  });
-  console.log($('.bob'))
+nowColor()
+init()
